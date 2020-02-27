b668058bff5392e1c54b90510511d49d2717e834
Thiago Giammattey <giammattey@gmail.com>
2020-02-27 14:52:07 -0300

    adiciona formulário de pedido de recuperação de senha

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -10,5 +10,9 @@ module.exports = {
 	logout(req, res) {
 		req.session.destroy()
 		return res.redirect('/')
-	}
+	},
+	forgotForm(req, res) {
+		return res.render('session/forgot-password')
+	},
+	forgot(req, res) {}
 }
--- src/routes/users.js
+++ src/routes/users.js
@@ -7,7 +7,10 @@ const UserController = require('../app/controllers/UserController')
 const UserValidator = require('../app/validators/user')
 const SessionValidator = require('../app/validators/session')
 
-const { isLoggedRedirectToUsers } = require('../app/middlewares/session')
+const {
+	isLoggedRedirectToUsers,
+	onlyUsers
+} = require('../app/middlewares/session')
 
 // Login/Logout
 routes.get('/login', isLoggedRedirectToUsers, SessionController.loginForm)
@@ -15,15 +18,19 @@ routes.post('/login', SessionValidator.login, SessionController.login)
 routes.post('/logout', SessionController.logout)
 
 // Reset Password / Forgot Password
-// routes.get('/forgot-password', SessionController.forgotForm)
+routes.get('/forgot-password', SessionController.forgotForm)
 // routes.get('/password-reset', SessionController.resetForm)
-// routes.post('/forgot-password', SessionController.forgot)
+routes.post(
+	'/forgot-password',
+	SessionValidator.forgot,
+	SessionController.forgot
+)
 // routes.post('/password-reset', SessionController.reset)
 
 routes.get('/register', UserController.registerForm)
 routes.post('/register', UserValidator.post, UserController.post)
 
-routes.get('/', UserValidator.show, UserController.show)
+routes.get('/', onlyUsers, UserValidator.show, UserController.show)
 routes.put('/', UserValidator.update, UserController.update)
 // routes.delete('/', UserController.delete)
 
