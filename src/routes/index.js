18b2b39cecad15695579177cf133698e0fd5a230
Thiago Giammattey <giammattey@gmail.com>
2020-02-27 14:06:42 -0300

    bloqueio de rotas para usuários não cadastrados

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const routes = express.Router()
 
 const HomeController = require('../app/controllers/HomeController')
-const SearchController = require('../app/controllers/SearchController')
 
 const products = require('./products')
 const users = require('./users')
@@ -10,15 +9,12 @@ const users = require('./users')
 // Home
 routes.get('/', HomeController.index)
 
-// Search
-routes.get('/products/search', SearchController.index)
-
 routes.use('/products', products)
 routes.use('/users', users)
 
 // Aliases
 routes.get('/ads/create', function(req, res) {
-	return res.redirect('/produts/create')
+	return res.redirect('/products/create')
 })
 
 routes.get('/accounts', function(req, res) {
--- src/routes/products.js
+++ src/routes/products.js
@@ -4,8 +4,15 @@ const routes = express.Router()
 const multer = require('../app/middlewares/multer')
 
 const ProductController = require('../app/controllers/ProductController')
+const SearchController = require('../app/controllers/SearchController')
 
-routes.get('/create', ProductController.create)
+const { onlyUsers } = require('../app/middlewares/session')
+
+// Search
+routes.get('/search', SearchController.index)
+
+//Products
+routes.get('/create', onlyUsers, ProductController.create)
 routes.get('/:id', ProductController.show)
 routes.get('/:id/edit', ProductController.edit)
 routes.post('/', multer.array('photos', 6), ProductController.post)
