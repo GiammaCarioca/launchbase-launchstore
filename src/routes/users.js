f112554310e3f140c9a2c9e31f50fceac885f7fb
Thiago Giammattey <giammattey@gmail.com>
2020-02-27 14:19:07 -0300

    redirecionamento de usuários cadastrados

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,10 +7,14 @@ const UserController = require('../app/controllers/UserController')
 const UserValidator = require('../app/validators/user')
 const SessionValidator = require('../app/validators/session')
 
-routes.get('/login', SessionController.loginForm)
+const { isLoggedRedirectToUsers } = require('../app/middlewares/session')
+
+// Login/Logout
+routes.get('/login', isLoggedRedirectToUsers, SessionController.loginForm)
 routes.post('/login', SessionValidator.login, SessionController.login)
 routes.post('/logout', SessionController.logout)
 
+// Reset Password / Forgot Password
 // routes.get('/forgot-password', SessionController.forgotForm)
 // routes.get('/password-reset', SessionController.resetForm)
 // routes.post('/forgot-password', SessionController.forgot)
